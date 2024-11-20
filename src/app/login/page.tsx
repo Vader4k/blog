4851521bf819acb51849008e110025cb21a61503
Vader4k <bigkingkush295@gmail.com>
2024-11-20 17:28:06 +0100

    @add/ made login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Image from 'next/image'
+import LoginWGoogle from '@/components/LoginWGoogle'
+import LoginWGithub from '@/components/LoginWGithub'
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div className='h-screen w-full flex flex-col items-center justify-center'>
+      <div className='flex items-center gap-2'>
+        <Image src={'/logo.png'} width={50} height={50} alt='logo'/>
+        <h2 className='font-medium text-lg'>Welcome back!</h2>
+      </div>
+      <div className='py-5 w-full space-y-5'>
+        <LoginWGoogle />
+        <LoginWGithub />
+      </div>
+    </div>
   )
 }
 
