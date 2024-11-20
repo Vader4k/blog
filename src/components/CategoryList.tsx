ad8728386e2a0828ffdc1636690ee93e3d531ee6
Vader4k <bigkingkush295@gmail.com>
2024-11-20 07:55:30 +0100

    @add/ added category component

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+/* eslint-disable @next/next/no-img-element */
+import { categories } from "@/lib/categories";
+
+const colorMap: { [key: string]: string } = {
+  'pink-300': '#FBBFCA',
+  'yellow-300': '#FCE',
+  'brown-200': '#D9CBA0',
+  'yellow-200': '#FCE68A',
+  'blue-300': '#93C5FD',
+};
 
 const CategoryList = () => {
-  return (
-    <div>CategoryList</div>
-  )
-}
 
-export default CategoryList
+  return (
+    <div className="py-10">
+      <h1 className="font-semibold text-4xl">Popular Categories</h1>
+      <div className="flex justify-between py-14">
+        {categories.map((category) => (
+          <span
+            className="flex items-center gap-3 px-16 py-4 rounded-lg"
+            key={category.id}
+            style={{ backgroundColor: colorMap[category.color] }}
+          >
+            <img
+              src={category.image}
+              className="rounded-full size-[40px] object-cover"
+              alt={category.name + " icon"}
+            />
+            <p className="capitalize text-sm">{category.name}</p>
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CategoryList;
--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import Image from "next/image";
 
 const Featured = () => {
   return (
-    <div>Featured</div>
-  )
-}
+    <div className="py-10">
+      <h1 className="text-7xl">
+        <b>Hey, Vader here!</b> Discover my stories and creative ideas.
+      </h1>
+      <div className="mt-16 flex gap-10 items-center">
+        <div className="flex-1 relative min-h-[500px]">
+          <Image src={'/44775.jpg'} fill alt="venice"/>
+        </div>
+        <div className="flex-1 items-start flex flex-col gap-5">
+          <h1 className="text-4xl font-medium">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Voluptates, quas.
+          </h1>
+          <p className="text-softTextColor">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+            exercitationem corrupti dolorem eveniet blanditiis amet in tenetur
+            architecto atque recusandae, maxime libero excepturi debitis
+            similique voluptatem, iusto unde at omnis!
+          </p>
+          <button className="px-4 py-3 w-fit bg-gray-300 text-sm font-thin rounded-md">Read More</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Featured
+export default Featured;
